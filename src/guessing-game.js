b5f33d0875aadeb9e9a7c4f5c851afdde1c0a707
nikolairoshchin <99800316+nikolairoshchin@users.noreply.github.com>
2023-02-09 20:37:28 +0200

    feat: add GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        let min = 0;
+        let max = 0;
+        let mid = 0;
+    }
 
     setRange(min, max) {
-
+        if (min > max) return 'Error';
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.mid = Math.ceil((this.max+this.min)/2);
+        return this.mid;
     }
 
     lower() {
-
+        this.setRange(this.min, this.mid);
     }
 
     greater() {
-
+        this.setRange(this.mid, this.max);
     }
 }
 
